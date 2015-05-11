0849cd3e55a9361733ce88bc3d27a7f94ecc8c47
NicholasHansen <nicholasfhansen@gmail.com>
2015-05-11 18:12:44 +0200

    Ørns working camera addition.

--- app/src/main/java/com/kea/sillygamers/unlocksmith/MainActivity.java
+++ app/src/main/java/com/kea/sillygamers/unlocksmith/MainActivity.java
@@ -4,12 +4,11 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View; //the import for the onClick listeners. allows us to use items on screen
-import android.widget.ImageView;
+import android.view.View;
 import android.widget.TextView;
 
 
@@ -18,6 +17,7 @@ public class MainActivity extends ActionBarActivity {
 
     //PlayerCharacter thisCharacter = new PlayerCharacter();
 
+    public final int QR_MESSAGE = 1001;
     TextView locksmithLevel;
     public String skillLevelCount;
     @Override
@@ -67,29 +67,36 @@ public class MainActivity extends ActionBarActivity {
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-            if(resultCode == RESULT_OK){
+        if(resultCode == RESULT_OK){
 
-                TextView tvLevelCounter = (TextView) findViewById(R.id.tvLevelCounter);
+            TextView tvLevelCounter = (TextView) findViewById(R.id.tvLevelCounter);
 
-                SharedPreferences sharedPreferences=getSharedPreferences("MyData", Context.MODE_PRIVATE);
-                skillLevelCount = sharedPreferences.getString("locksmithLevel", "0");
-                locksmithLevel.setText(skillLevelCount);
-            }
+            SharedPreferences sharedPreferences=getSharedPreferences("MyData", Context.MODE_PRIVATE);
+            skillLevelCount = sharedPreferences.getString("locksmithLevel", "0");
+            locksmithLevel.setText(skillLevelCount);
+        }
+
+        if (resultCode == QR_MESSAGE)
+        {
+            TextView tvLevelCounter = (TextView) findViewById(R.id.tvLevelCounter);
+            String level = (String) tvLevelCounter.getText();
+            String scanInput = data.getStringExtra("value");
+
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+            alertDialogBuilder.setView(Validation.checkLock(scanInput, level, MainActivity.this));
+
+            AlertDialog dialog = alertDialogBuilder.create();
+            dialog.show();
+        }
+
 
 
     }//onActivityResult
 
     public void scanQrCode (View v)
     {
-        TextView tvLevelCounter = (TextView) findViewById(R.id.tvLevelCounter);
-        String level = (String) tvLevelCounter.getText();
-        String scanInput = "5";
-
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
-        alertDialogBuilder.setView(Validation.checkLock(scanInput, level, MainActivity.this));
-
-        AlertDialog dialog = alertDialogBuilder.create();
-        dialog.show();
+        Intent intent = new Intent(this, CameraTestActivity.class);
+        startActivityForResult(intent,QR_MESSAGE);
 
 
     }
